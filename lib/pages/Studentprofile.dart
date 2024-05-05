898167f9f1c9ea33cbc5f0b9688d4490a24ed8ae
Rochan Arya <rochanarya@gmail.com>
2024-05-05 11:56:57 +0530

    New Commit

--- lib/pages/Studentprofile.dart
+++ lib/pages/Studentprofile.dart
@@ -12,7 +12,7 @@ class _TransferpageState extends State<Transferpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
       body: SingleChildScrollView(
         child: Center(
           child: Padding(
@@ -21,15 +21,213 @@ class _TransferpageState extends State<Transferpage> {
             ),
             child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 330, top: 40),
-                  child: Icon(
-                    Icons.arrow_back_ios,
-                    color: Color.fromARGB(255, 228, 228, 228),
+                const SingleChildScrollView(
+                  child: InkWell(
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 330, top: 40),
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        color: Color.fromARGB(255, 228, 228, 228),
+                      ),
+                    ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 50, top: 35),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 35, horizontal: 20),
+                    child: Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 14, 25, 32),
+                              borderRadius: BorderRadius.circular(10)),
+                          height: 150,
+                          width: 180,
+                          child: Column(
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.only(right: 105, top: 15),
+                                child: Text(
+                                  'TOTAL',
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 228, 228, 228),
+                                      fontWeight: FontWeight.w900,
+                                      fontSize: 16),
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(right: 70, top: 10),
+                                child: Text(
+                                  'STUDENTS',
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 228, 228, 228),
+                                      fontWeight: FontWeight.w900,
+                                      fontSize: 16),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 80),
+                                child: Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(100),
+                                      border: Border.all(
+                                          color: const Color.fromARGB(
+                                              255, 56, 250, 82),
+                                          width: 3)),
+                                  child: const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 10, horizontal: 12),
+                                    child: Text(
+                                      '71',
+                                      style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 228, 228, 228),
+                                          fontWeight: FontWeight.w900,
+                                          fontSize: 16),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 14, 25, 32),
+                              borderRadius: BorderRadius.circular(10)),
+                          height: 150,
+                          width: 180,
+                          child: Column(
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.only(right: 105, top: 15),
+                                child: Text(
+                                  'TOTAL',
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 228, 228, 228),
+                                      fontWeight: FontWeight.w900,
+                                      fontSize: 16),
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(right: 70, top: 10),
+                                child: Text(
+                                  'BOYS',
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 228, 228, 228),
+                                      fontWeight: FontWeight.w900,
+                                      fontSize: 16),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 80),
+                                child: Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(100),
+                                      border: Border.all(
+                                          color: const Color.fromARGB(
+                                              255, 68, 171, 255),
+                                          width: 3)),
+                                  child: const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 10, horizontal: 12),
+                                    child: Text(
+                                      '51',
+                                      style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 228, 228, 228),
+                                          fontWeight: FontWeight.w900,
+                                          fontSize: 16),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 14, 25, 32),
+                              borderRadius: BorderRadius.circular(10)),
+                          height: 150,
+                          width: 180,
+                          child: Column(
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.only(right: 105, top: 15),
+                                child: Text(
+                                  'TOTAL',
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 228, 228, 228),
+                                      fontWeight: FontWeight.w900,
+                                      fontSize: 16),
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(right: 110, top: 10),
+                                child: Text(
+                                  'GIRLS',
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 228, 228, 228),
+                                      fontWeight: FontWeight.w900,
+                                      fontSize: 16),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 80),
+                                child: Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(100),
+                                      border: Border.all(
+                                          color: const Color.fromARGB(
+                                              255, 252, 104, 255),
+                                          width: 3)),
+                                  child: const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 10, horizontal: 12),
+                                    child: Text(
+                                      '14',
+                                      style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 228, 228, 228),
+                                          fontWeight: FontWeight.w900,
+                                          fontSize: 16),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 50, top: 25),
                   child: Row(
                     children: [
                       Text(
@@ -42,28 +240,28 @@ class _TransferpageState extends State<Transferpage> {
                     ],
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 40,
                 ),
-                MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
-                MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MyblackContainer(Name: 'Meyyapan', Cgpa: '10'),
+                const MygreyContainer(Name: 'Meyyapan', Cgpa: '10'),
               ],
             ),
           ),
